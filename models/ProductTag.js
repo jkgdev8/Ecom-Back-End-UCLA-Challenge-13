aff73e6e1adff691a6db0c4a82a890b063252f9a
Jkgdev8 <juliennegarcia@Juliennes-MBP.socal.rr.com>
2022-01-25 16:17:49 -0800

    models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,15 +6,37 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+  // define an id column
+  id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
   },
-  {
+  // define product_id column
+  product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: "product",
+          key: "id"
+      }
+  },
+  // define tag_id column
+  tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: "tag",
+          key: "id"
+      }
+  }
+},
+{
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'product_tag',
-  }
+}
 );
 
 module.exports = ProductTag;
